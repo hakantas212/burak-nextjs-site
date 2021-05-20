4176387f646a8189c75cab6b3a60f060bbe00502
Hakan Tas <hakantas212@gmail.com>
2021-05-20 17:42:38 +0200

    footer changes

--- components/footer.js
+++ components/footer.js
@@ -9,22 +9,30 @@ export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-row lg:flex-row">
-          <div className="flex flex-col lg:flex-col justify-center lg:w-1/2">
-              <h3 className="text-4xl py-4 lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-                  {
-                    <a href={`mailto:${data.footer.email}`}>Get in touch</a> 
-                  }
+        <div className="py-28 flex flex-col lg:flex-col items-center">
+          <div className="flex flex-col lg:flex-col justify-center items-center text-center lg:w-1/2">
+              <h3 className="text-4xl py-4 lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-center mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+                Get in touch
               </h3>
-              <h4>{data.footer.footerContent}</h4>
+              <h4 className="py-5">{data.footer.footerContent}</h4>
           </div>
-          <div className="w-8 flex flex-col lg:flex-col justify-center lg:w-1/2">
+          <div className="w-8 flex flex-row lg:flex-row justify-center lg:w-1/2">
           { data.footer.footerLinks &&
-               <Link href={data.footer.footerLinks.socialMediaLink}>
-               <img className="object-contain h-48 w-full" src={data.footer.footerLinks.socialMediaIcon.fields.file.url}></img>
-             </Link>
+            <a target="_blank" href={data.footer.footerLinks.socialMediaLink}>
+              <img className="object-contain h-9 w-10" src={data.footer.footerLinks.socialMediaIcon.fields.file.url}></img>
+            </a>
           }
+          {
+            <a className="p-0 m-0 w-12 h-12" href={`mailto:${data.footer.email}`}>
+              <svg width="100" height="80" xmlns="http://www.w3.org/2000/svg" className="bg-transparent object-contain flex items-center text-blue-700 border-blue-500" fill="none" viewBox="0 0 40 40" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+              </svg>
+            </a> 
+            
+          }
+
           </div>
+
         </div>
       </Container>
     </footer>
