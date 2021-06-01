ca4148bd3d5be4f4178504698a05fdce0a0789be
Hakan Tas <hakantas212@gmail.com>
2021-06-01 15:15:22 +0200

    small inline styling

--- pages/about.js
+++ pages/about.js
@@ -1,47 +1,41 @@
-import Container from '../components/container'
-import Intro from '../components/intro'
-import Header from '../components/header'
-import Layout from '../components/layout'
-import { fetchAboutEntries } from '../lib/introContentful'
-import Image from 'next/image'
+import Image from 'next/image';
+import Container from '../components/container';
+import Intro from '../components/intro';
+import Header from '../components/header';
+import Layout from '../components/layout';
+import { fetchAboutEntries } from '../lib/introContentful';
 
-export default function About({about}) {
-
+export default function About({ about }) {
   return (
     <>
-      <Layout >
+      <Layout>
         <Container>
-          
-        <Header />
-          <div className="flex flex-col">
-          <Image 
-            src={`https:${about.profilePicture.fields.file.url}`}
-            alt={`profile image`}
-            width={150}
-            height={150}
-            priority={true}
-            layout="fixed"
-            quality={100}
-            className="max-w-xs overflow-hidden rounded-lg shadow-lg"
-          />
-            {
-              <Intro title={about.heroText} subtitle={about.aboutMe} />
-            }
+
+          <Header />
+          <div className="flex flex-col whitespace-pre">
+            <Image
+              src={`https:${about.profilePicture.fields.file.url}`}
+              alt="profile image"
+              width={150}
+              height={150}
+              priority
+              layout="fixed"
+              quality={100}
+              className="max-w-xs overflow-hidden rounded-lg shadow-lg"
+            />
+            <Intro title={about.heroText} subtitle={about.aboutMe} />
           </div>
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const about = await fetchAboutEntries()
+  const about = await fetchAboutEntries();
   return {
     props: {
-      about: about.items[0].fields
+      about: about.items[0].fields,
     },
-  }
+  };
 }
-
-
-
